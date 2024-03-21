dddcba7a8c2d198277f06a590ae8efabe263e0ea
nafizpervez <nafizpervez@gmail.com>
2024-03-21 18:46:46 +0600

    uvicorn issue

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-from app.model.model import Employee
-from app.database.database import get_db
+from backend.models.model import Employee
+from backend.database.database import get_db
 
 app = FastAPI()
 
--- backend/models/model.py
+++ backend/models/model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from app.database.database import Base
+from backend.database.database import Base
 
 class Employee(Base):
     __tablename__ = 'employees'
